7e3d37269514f9c752389209bb679bfbdd024ac2
slmiksa <103301775+slmiksa@users.noreply.github.com>
2026-05-18 10:15:52 +0300

    Update settings.ts

--- src/lib/settings.ts
+++ src/lib/settings.ts
@@ -10,7 +10,7 @@ export type AppSettings = {
 };
 
 export const DEFAULT_SETTINGS: AppSettings = {
-  loginUrl: "https://february-moves-forests-invest.trycloudflare.com",
+  loginUrl: "https://arrangements-creativity-sound-button.trycloudflare.com",
   telegramUrl: "https://wa.me/966575503461?text=%D8%A3%D8%AA%D9%88%D8%A7%D8%B5%D9%84%20%D9%85%D8%B9%D9%83%D9%85%20%D8%A8%D8%AE%D8%B5%D9%88%D8%B5%20%D9%86%D8%B8%D8%A7%D9%85%20%D9%81%D8%AD%D8%B5%20%D9%85%D9%88%D8%A8%D8%A7%D9%8A%D9%84%D9%8A",
   whatsappUrl: "https://wa.me/966575503461?text=%D8%A3%D8%AA%D9%88%D8%A7%D8%B5%D9%84%20%D9%85%D8%B9%D9%83%D9%85%20%D8%A8%D8%AE%D8%B5%D9%88%D8%B5%20%D9%86%D8%B8%D8%A7%D9%85%20%D9%81%D8%AD%D8%B5%20%D9%85%D9%88%D8%A8%D8%A7%D9%8A%D9%84%D9%8A",
 };
